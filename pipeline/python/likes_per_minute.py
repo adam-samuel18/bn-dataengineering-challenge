76d3393720c52de06e32a5c49e0f7f4247f162fe
adam-samuel18 <96579231+adam-samuel18@users.noreply.github.com>
2025-03-16 23:52:52 +0000

    created function transform_likes_per_minute

--- pipeline/python/likes_per_minute.py
+++ pipeline/python/likes_per_minute.py
@@ -7,28 +7,34 @@ output to a csv file.
 
 import polars as pl
 
-# The schema specifies the desired fields and datatypes from the bluesky.events file
-# Then a dataframe df is created from the json file containing these desired fields
+def transform_likes_per_minute():
 
-schema = {
-    'timestamp': pl.Datetime,
-    'path': pl.String
-}
+    # The schema specifies the desired fields and datatypes from the
+    # bluesky.events file. Then a dataframe df is created from the json file
+    # containing these desired fields
 
-df = pl.read_ndjson('bluesky.events', schema = schema)
+    schema = {
+        'timestamp': pl.Datetime,
+        'path': pl.String
+    }
 
-# The columns 'ts_min' and 'event_name' are added to the dataframe
-# The dataframe is then filtered to only show like events
+    df = pl.read_ndjson('bluesky.events', schema = schema)
 
-df = df.with_columns(
-    pl.col('timestamp').dt.truncate('1m').alias('ts_min'),
-    pl.col('path').str.split('/').list.get(0).
-        str.split('.').list.get(3, null_on_oob = True).alias('event_name')
-)
+    # The columns 'ts_min' and 'event_name' are added to the dataframe
+    # The dataframe is then filtered to only show like events
 
-df = df.filter(pl.col('event_name') == 'like')
+    df = df.with_columns(
+        pl.col('timestamp').dt.truncate('1m').alias('ts_min'),
+        pl.col('path').str.split('/').list.get(0).
+            str.split('.').list.get(3, null_on_oob = True).alias('event_name')
+    )
 
-# Finally the likes per minute is calculated and the data is written to csv
+    df = df.filter(pl.col('event_name') == 'like')
 
-df = df.group_by('ts_min').len().sort('ts_min')
-df.write_csv('./metrics/likes_per_minute_python.csv')
+    # Finally the likes per minute is calculated and the data is written to csv
+
+    df = df.group_by('ts_min').len().sort('ts_min')
+    df.write_csv('./metrics/likes_per_minute_python.csv')
+
+if __name__ == "__main__":
+    transform_likes_per_minute()
